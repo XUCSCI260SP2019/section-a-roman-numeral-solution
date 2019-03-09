08e7f29f07784dca885472dfaf3a626fd2e4c676
dan <Den>
2019-03-09 15:32:36 -0500

    initial encoder

--- src/test/java/edu/xavier/csci/dentinger/RomanNumeralConverterTest.java
+++ src/test/java/edu/xavier/csci/dentinger/RomanNumeralConverterTest.java
@@ -1,42 +1,82 @@
 package edu.xavier.csci.dentinger;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 public class RomanNumeralConverterTest {
 
+    private RomanNumeralConverter sut;
+
+    @Before
+    public void setup() {
+        sut = new RomanNumeralConverter();
+    }
+
     @Test
     public void decodeI() {
-        RomanNumeralConverter r = new RomanNumeralConverter();
-        assertEquals(Integer.valueOf(1), r.decode("I"));
+
+        assertEquals(Integer.valueOf(1), sut.decode("I"));
     }
 
     @Test
     public void decodei() {
-        RomanNumeralConverter r = new RomanNumeralConverter();
-        assertEquals(Integer.valueOf(1), r.decode("i"));
+
+        assertEquals(Integer.valueOf(1), sut.decode("i"));
     }
 
     @Test
     public void decodeII() {
-        RomanNumeralConverter r = new RomanNumeralConverter();
-        assertEquals(Integer.valueOf(2), r.decode("II"));
+
+        assertEquals(Integer.valueOf(2), sut.decode("II"));
     }
 
     @Test
     public void decodeXI() {
-        RomanNumeralConverter r = new RomanNumeralConverter();
-        assertEquals(Integer.valueOf(11), r.decode("XI"));
+
+        assertEquals(Integer.valueOf(11), sut.decode("XI"));
     }
 
     @Test
     public void decodeXXI() {
-        RomanNumeralConverter r = new RomanNumeralConverter();
-        assertEquals(Integer.valueOf(21), r.decode("XXI"));
+
+        assertEquals(Integer.valueOf(21), sut.decode("XXI"));
     }
     @Test
     public void decodeMMXIX() {
-        RomanNumeralConverter r = new RomanNumeralConverter();
-        assertEquals(Integer.valueOf(2019), r.decode("MMXIX"));
+
+        assertEquals(Integer.valueOf(2019), sut.decode("MMXIX"));
     }
 
+    @Test
+    public void encodeOne() {
+        assertThat(sut.encode(1), is("I"));
+    }
+    @Test
+    public void encodeTwo() {
+        assertThat(sut.encode(2), is("II"));
+    }
+    @Test
+    public void encodeFive() {
+        assertThat(sut.encode(5), is("V"));
+    }
+    @Test public void encodeTen() {
+        assertThat(sut.encode(10), is("X"));
+    }
+    @Test public void encodeFiveHundred() {
+        assertThat(sut.encode(500), is("D"));
+    }
+    @Test public void encodeOneK() {
+        assertThat(sut.encode(1000), is("M"));
+    }
+    @Test public  void encode1111() {
+        assertThat(sut.encode(1111), is("MCXI"));
+    }
+    @Test public void encode4() {
+        assertThat(sut.encode(4), is("IV"));
+    }
+    @Test public void encode9() {
+        assertThat(sut.encode(9), is("IX"));
+    }
 }
